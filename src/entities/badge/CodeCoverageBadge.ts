2b1da8e63db52db793f64921835483359336b2ad
Max Lehmann <85064456+MaxLehmann01@users.noreply.github.com>
2025-07-07 00:50:48 +0200

    Changed: adjusted coverage badge color breakpoints

--- src/entities/badge/CodeCoverageBadge.ts
+++ src/entities/badge/CodeCoverageBadge.ts
@@ -7,15 +7,31 @@ export default class CodeCoverageBadge extends AbstractBadge {
         this.label = 'Code Coverage';
         this.value = `${coverage}%`;
 
-        if (coverage <= 50) {
+        if (coverage < 60) {
             this.color = '#e05d44';
         }
 
-        if (coverage > 50 && coverage <= 80) {
-            this.color = '#dab639';
+        if (coverage >= 60 && coverage < 65) {
+            this.color = '#c66a0e';
         }
 
-        if (coverage > 80) {
+        if (coverage >= 65 && coverage < 70) {
+            this.color = '#c6980f';
+        }
+
+        if (coverage >= 70 && coverage < 75) {
+            this.color = '#c6c60d';
+        }
+
+        if (coverage >= 75 && coverage < 80) {
+            this.color = '#98c611';
+        }
+
+        if (coverage >= 80 && coverage < 85) {
+            this.color = '#6ac611';
+        }
+
+        if (coverage >= 85) {
             this.color = '#4c1';
         }
     }
